af85df9571588a4acd146fee80cdb2a898564a6c
unknown <bart.sniatkowski@gmail.com>
2024-03-12 11:10:41 +0100

    og meta check

--- src/renderer/+onRenderHtml.tsx
+++ src/renderer/+onRenderHtml.tsx
@@ -27,6 +27,9 @@ const onRenderHtml: OnRenderHtmlAsync = async (pageContext) => {
             <title>Z Nosem W Piwie</title>
             <meta name="description" content="Welcome to fictious entity 'Z Nosem W Piwie' - your virtual haven where the spirit of conviviality meets the charm of a bygone era! Immerse yourself in a whimsical fusion of rustic charm and vibrant energy, where every visit feels like a journey back in time. Join us for imaginative events, curated menus, and unforgettable moments in our fictitious pub/cafe. Escape reality and embrace the magic of make-believe at 'Z Nosem W Piwie.'">
             <meta name="theme-color" content="#FFE0B2"/>
+            <meta property="og:title" content="Z Nosem W Piwie">
+            <meta property="og:description" content="A fictious entity 'Z Nosem W Piwie'">
+            <meta property="og:image" content="/z-nosem-w-piwie-react/logo.svg">
             ${dangerouslySkipEscape(sheet.getStyleTags())}
         </head>
       <body style="overflow: hidden;">
